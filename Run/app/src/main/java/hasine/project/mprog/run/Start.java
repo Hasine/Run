f8cb1d7e2a9613387f49a1c71859d26ae6016c92
Hasine Efeturk <hasineefeturk@hotmail.com>
2016-01-05 23:17:04 +0100

    ..

--- Run/app/src/main/java/hasine/project/mprog/run/Start.java
+++ Run/app/src/main/java/hasine/project/mprog/run/Start.java
@@ -6,16 +6,16 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class RunMainActivity extends AppCompatActivity {
+public class Start extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_run_main);
+        setContentView(R.layout.activity_start);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -27,17 +27,6 @@ public class RunMainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
-
-
-    public void search(View view) {
-        Intent gotoSearch = new Intent(this, Search.class);
-        startActivity(gotoSearch);
-    }
-
-    public void start(View view) {
-        Intent gotoStart = new Intent(this, Start.class);
-        startActivity(gotoStart);
     }
 
     @Override
